759b18aaa4410e575ce83dbf7cb1bbf1cbbdc011
amitshah326 <amitshah@umich.edu>
2015-02-27 20:47:04 -0500

    added form for rsa with validation

--- js/main.js
+++ js/main.js
@@ -7,10 +7,38 @@ rfa.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
         when('/queue', {
-            templateUrl: 'pages/queue.html',
+            templateUrl: 'pages/form.html',
         })
         .otherwise({
             redirectTo: '/queue'
         });
     }]
 );
+
+
+rfa.controller('formController', formController);
+
+// define angular module/app
+function formController($scope, $http) {
+    $scope.formData = {};
+    // process the form
+    $scope.submitForm = function() {
+        $http({
+            method  : 'POST',
+            url     : 'pages/confirmation.php',
+            data    : $.param($scope.formData),  // pass in data as strings
+            headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+        })
+            .success(function(data) {
+                console.log(data);
+                if (!data.success) {
+                    // if not successful, bind errors to error variables
+                    $scope.errorrfatype = data.errors.rfatype;
+                    $scope.errordescrip = data.errors.descrip;
+                } else {
+                    // if successful, bind success message to message
+                    $scope.message = data.message;
+                }
+            });
+    };
+}
---
+++ pages/confirmation.php
@@ -0,0 +1,22 @@
+<?php
+
+$errors         = array();  	// array to hold validation errors
+$data 			= array(); 		// array to pass back data
+// validate the variables ======================================================
+	if (empty($_POST['rfatype']))
+		$errors['rfatype'] = 'A RFA must be selected.';
+	if (empty($_POST['descrip']))
+		$errors['descrip'] = 'Description is required.';
+// return a response ===========================================================
+	// response if there are errors
+	if ( ! empty($errors)) {
+		// if there are items in our errors array, return those errors
+		$data['success'] = false;
+		$data['errors']  = $errors;
+	} else {
+		// if there are no errors, return a message
+		$data['success'] = true;
+		$data['message'] = 'Success!';
+	}
+	// return all our data to an AJAX call
+	echo json_encode($data);
